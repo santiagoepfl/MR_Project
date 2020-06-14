b74649a62f1025f92afda8092620030050b615a3
unknown <santiago.antonmoreno@epfl.ch>
2020-06-14 16:35:07 +0200

    day choosing and model fitting with some overdispersion test

--- Project_Corona_Main.R
+++ Project_Corona_Main.R
@@ -7,6 +7,8 @@
 
 library(mgcv)
 
+library("AER")
+
 source("basic_analysis.R")
 
 set.seed(2)
@@ -26,79 +28,64 @@ plot.Country("United_States_of_America",plot=TRUE)
 plot.Country("China",plot=TRUE)
 
 
-#we need to find the end of the initial phase
+###############################################
 #Fitted Splines and slopes
 first_day_china<-first_day_epidemic.death("China")
 Death_china<-DeathsByCountry["China",]
 n.days <- length(Death_china)
 x <- c(1:n.days)
-# je pense sa fait pas beaucoup de sens de fit comme ça la spline
 spline_fit_china<-gam(Death_china[first_day_china:n.days]~s(x[first_day_china:n.days]),family=poisson(link="log"))$fit
 slope_china<-diff(diff(spline_fit_china))#Double derivative to find when slop stops increasing
-slope_china
+slope_china #the initial phase lasted about 19 days
 
 
 first_day_germany<-first_day_epidemic.death("Germany")
-
 Death_germany<-DeathsByCountry["Germany",]
-n.days<-length(Death_germany)
-x <- c(1:n.days) 
 spline_fit_germany<-gam(Death_germany[first_day_germany:n.days]~s(x[first_day_germany:n.days]),family=poisson(link="log"))$fit
-
-# bon ducoup sa marche plus si on prend la bonne taille
-
 slope_germany<-diff(diff(spline_fit_germany))#Double derivative to find when slop stops increasing
 slope_germany#around 22 negative values start.
-DeathsByCountry["Germany",]
 
-#first_day_germany+22=92
-germany.gam<-gam(Death_germany[first_day_germany:92]~x[first_day_germany:92],family=poisson(link="log"))
+first_day_france<-first_day_epidemic.death("France")
+Death_france<-DeathsByCountry["France",]
+spline_fit_france<-gam(Death_france[first_day_france:n.days]~s(x[first_day_france:n.days]),family=poisson(link="log"))$fit
+slope_france<-diff(diff(spline_fit_france))
+slope_france # 30 days initial phase
+
+first_day_usa<-first_day_epidemic.death("United_States_of_America")
+Death_usa<-DeathsByCountry["United_States_of_America",]
+spline_fit_usa<-gam(Death_usa[first_day_usa:n.days]~s(x[first_day_usa:n.days]),family=poisson(link="log"))$fit
+slope_usa<-diff(diff(spline_fit_usa))
+slope_usa # 38 days initial phase
+
+
+
+#################################################
+#Poisson fitting for each country
+germany.gam<-gam(Death_germany[first_day_germany:93]~x[first_day_germany:93],family=poisson(link="log"))
 summary(germany.gam)
+germany.gam$aic
+dispersiontest(germany.gam,trafo=1) #overdispersion test there's no significant overdispersion in germany
 
 
-diff(diff(smooth.spline(x[first_day_germany:n.days],Death_germany[first_day_germany:n.days])$y))
+china.gam<-gam(Death_china[first_day_china:40]~x[first_day_china:40],family = poisson(link="log"))
+summary(china.gam) #less variance explained than germany
+china.gam$aic
+dispersiontest(china.gam,trafo = 1) # there is no significant evidence for overdispersion
 
-smooth.spline(x[first_day_germany:n.days],Death_germany[first_day_germany:n.days])$y
-first_day_France<-first_day_epidemic.death("France")
+france.gam<-gam(Death_france[first_day_france:94]~x[first_day_france:94],family = poisson())
+summary(france.gam)
+france.gam$aic #really bad aic
+dispersiontest(france.gam,trafo = 1) # there is clearly overdispersion
+france.gam2<-gam(Death_france[first_day_france:94]~x[first_day_france:94],family = quasipoisson())
+summary(france.gam2) #the coefficient are the same but more confidence except the scale parameter
 
-Death_France<-DeathsByCountry["France",]
+usa.gam<-gam(Death_usa[first_day_usa:100]~x[first_day_usa:100],family = poisson())
+summary(usa.gam)
+usa.gam$aic #auwfull aic
+dispersiontest(usa.gam,trafo=1) #clearly overdispersion
+usa.gam2<-gam(Death_usa[first_day_usa:100]~x[first_day_usa:100],family = quasipoisson())
+summary(usa.gam2) #estimates are also the same
 
-
-
-first_day_USA<-first_day_epidemic.death("United_States_of_America")
-
-
-
-##### Analysis after exploratory and initial phase finding
-country<-"France"
-y <- DeathsByCountry[country,]
-m <- CasesByCountry[country,]
-x <- c(1:n.days)
-y
-m#avec la mise a jour que j'ai faite y'a des cas negatif donc faudras gere ça
-
-#avec le s, ca fait un bail de smooth mais il y quand même un rapport avec poisson/quasipoisson
-gam(m~s(x),family=poisson(link="log"))
-gam(m~s(x),family=poisson(link="log"))$fit
-gam(m~s(x),family=poisson(link="log"))$smooth#les termes de smooth, rien compris.
-gam(m~s(x),family=poisson(link="log"))$formula #ca veut pas donner les formules rip
-
-plot(gam(m~s(x),family=poisson(link="log")))#apparemment ca fait le plot que quand il y a s(x), intéressant.
-
-
-#je pense que c'est le machin de poisson normale avec summary, coefs et le trucs fitted
-summary(gam(m~x,family=poisson(link="log")))
-gam(m~x,family=poisson(link="log"))$coefficients
-gam(m~x,family=poisson(link="log"))$fit
-
-
-#E(Yt)=m*exp(alpha+beta*t) (attention ici m c'est la population totale, pas la response.)
-#log(Yt)=log(m)+alpha+beta*t+g(t) (g(t) c'est l'éventuel smooth term,alpha intercep et beta c'est le coef de t)
-#Je pense que mon machin en dessous c'est la bonne formule avec l'éventuel smooth term s(x) ou il faut mettre bonne option.
-#On trouve le log(m)(m=population), on le soustrait à intercept et ça donne alpha vu que c'est constant
-gam(m~x+s(x),family=poisson(link = "log"))
-
-y
 
 # Explication Santi le s(x) en gros c'est un smooth term mais le default c'est pas un natural cubic spline
 #c'est un thin plate spline pas trop capté mais c'est un peu different du cours quoi
